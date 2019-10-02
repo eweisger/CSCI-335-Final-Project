a0402e0e0e5651730cb33c0a6a39f22834f832a1
Momentopolis <eweisger@unca.edu>
2019-10-02 13:18:51 -0400

    Deleted old folder containing port scanner files

--- finalProject/myThreads.h
+++
@@ -1,8 +0,0 @@
-#ifndef myThread_h
-#define myThread_h
-
-#include "linkedList.h"
-
-void *my_thread(linked_list* ll);
-
-#endif
--- finalProject/scanner.h
+++
@@ -1,6 +0,0 @@
-#ifndef scanner_h
-#define scanner_h
-
-void my_scanner(node* n, char* service, char* data);
-
-#endif
